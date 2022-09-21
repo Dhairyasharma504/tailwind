bbce620ba5253b2d4d33ecdf01911011a53d3b8a
Dhairyasharma504 <dhairyasharma2311@gmail.com>
2022-09-21 22:04:20 +0530

    done

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Footer = () => {
   return (
     <div>
-      <div class="flex items-center justify-center mt-10 mb-7 ">
+      <div class="flex items-center justify-center mt-10 mb-10 ">
         <p class="font-medium">© Copyright 1986. All Rights Reserved.</p>
       </div>
     </div>
