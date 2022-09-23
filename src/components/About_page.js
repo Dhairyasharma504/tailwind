4f1874d7484db64e856210c992f7cae8d3c6fac5
Dhairyasharma504 <dhairyasharma2311@gmail.com>
2022-09-23 12:48:53 +0530

    done

--- src/components/About_page.js
+++ src/components/About_page.js
@@ -3,7 +3,7 @@ import Image from './elements/Image';
 
 const About_page = () => {
   return (
-    <div className=" flex justify-center mt-5 ">
+    <div className=" flex justify-center container px-8 py-5 lg:py-8 mx-auto xl:px-5 max-w-screen-lg ">
       <div className="grid grid-cols-1 gap-14 lg:grid-cols-3 sm:grid-cols-2 ">
         <Image image="./images/aboutProfile1.webp" />
         <Image image="./images/aboutProfile2.webp" />
--- src/pages/about.js
+++ src/pages/about.js
@@ -10,21 +10,23 @@ const about = () => {
     <Layout>
       <Content title="About" subtitle="We are a small passionate team." />
       <About_page />
-      <h3 className="text-center py-8 ">
-        We provide real-time connectivity to enable software providers and
-        financial institutions to build integrated products for their small
-        business customers.
-      </h3>
-      <h3 className="text-center py-4 ">
-        Our API infrastructure is leveraged by clients ranging from lenders to
-        corporate card providers and business forecasting tools, with use cases
-        including automatic reconciliation, business dashboarding, and loan
-        decisioning.
-      </h3>
-      <div className="text-center ">
-        <Link href="/">
-          <a className="">Get in touch</a>
-        </Link>
+      <div className="text container px-8 py-5 lg:py-8 mx-auto xl:px-5 max-w-screen-lg ">
+        <h3 className="text-center py-4 text-base leading-loose ">
+          We provide real-time connectivity to enable software providers and
+          financial institutions to build integrated products for their small
+          business customers.
+        </h3>
+        <h3 className="text-center py-1 text-base leading-loose">
+          Our API infrastructure is leveraged by clients ranging from lenders to
+          corporate card providers and business forecasting tools, with use
+          cases including automatic reconciliation, business dashboarding, and
+          loan decisioning.
+        </h3>
+        <div className="text-center p-2  font-semibold">
+          <Link href="/">
+            <a className="">Get in touch</a>
+          </Link>
+        </div>
       </div>
     </Layout>
   );
