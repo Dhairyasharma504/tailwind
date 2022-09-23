7bf778eb0f97f5f3e5097efe3a99c43c0b2f17a6
Dhairyasharma504 <dhairyasharma2311@gmail.com>
2022-09-23 12:07:41 +0530

    about

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,3 +1,4 @@
+import Link from 'next/link';
 import React from 'react';
 import About_page from '../components/About_page';
 import Content from '../components/elements/Content';
@@ -9,6 +10,22 @@ const about = () => {
     <Layout>
       <Content title="About" subtitle="We are a small passionate team." />
       <About_page />
+      <h3 className="text-center py-8 ">
+        We provide real-time connectivity to enable software providers and
+        financial institutions to build integrated products for their small
+        business customers.
+      </h3>
+      <h3 className="text-center py-4 ">
+        Our API infrastructure is leveraged by clients ranging from lenders to
+        corporate card providers and business forecasting tools, with use cases
+        including automatic reconciliation, business dashboarding, and loan
+        decisioning.
+      </h3>
+      <div className="text-center ">
+        <Link href="/">
+          <a className="">Get in touch</a>
+        </Link>
+      </div>
     </Layout>
   );
 };
