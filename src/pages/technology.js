3e5e20eff44c375e181368d6cacdf24e40f3c7da
Dhairyasharma504 <dhairyasharma2311@gmail.com>
2022-09-23 20:43:51 +0530

    fix

--- src/pages/technology.js
+++ src/pages/technology.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import Layout from '../components/Layout';
+import Content from '../components/elements/Content';
 
 const technology = () => {
   return (
     <div>
       <Layout>
-        <div>fg</div>
+        <div className="container px-8 py-5 lg:py-8 mx-auto xl:px-5 max-w-screen-lg">
+          <p className="mt-5 text-xs font-medium text-blue-600 uppercase text-center">
+            Technology
+          </p>
+          <Content title="Architectural Engineering Wonders of the modern era for your Inspiration"></Content>
+        </div>
       </Layout>
     </div>
   );
